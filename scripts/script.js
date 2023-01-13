616901b734796df793ac4742499567a34a1ba23d
Angel Roberto <gelosgr@gmail.com>
2023-01-12 20:10:20 -0600

    añadi y use arrays al igual que termine las operaciones a usar

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
-	function comprarproducto(){
+	function calculos(){
+		let: calculos = [
+			" una vez ocurrido el descuento su objeto cuesta ", "una vez añadido el impuesto su objeto cuesta "
+		];
+
     let seleccion = prompt("bienvenido a calculadora de impuestos y descuentos! ingrese si o no").toUpperCase();
 	while (seleccion != "SI" && seleccion != "NO") {
 	alert("Dato incorrecto, por favor ingrese si o no").toUpperCase();
     };
 	if (seleccion === "SI") {
 alert("bienvenido!");
-let impuestodescuento = true;
-while (impuestodescuento) {
+let impuestoDescuento = true;
+while (impuestoDescuento) {
 	do {
 	calculo = prompt("¿necesitas calcular un descuento un impuesto?");
 
@@ -24,20 +28,31 @@ while (impuestodescuento) {
 } while (calculo != "descuento" && calculo != "DESCUENTO" && calculo != "impuesto" && calculo != "IMPUESTO");{
 
 if (calculo === "impuesto"){
-	let cantidad = parseInt(prompt("ingrese su cantidad 1"));
-	let cantidad1 = parseInt(prompt("ingrese su cantidad 2"));
+var impuesto1 = parseInt(prompt("ingrese el impuesto a sumar"));
+var impuesto2 = parseInt(prompt("ingrese el precio de su objeto"));
+let: operacionesImpuesto = [
+	impuestoR1 = impuesto1 / 100,
+	impuestoR2 = impuesto2 * impuestoR1,
+	impuestoR3 = impuesto2 + impuestoR2,
+]
+	let impu = calculos [1]
+	let opimpu = operacionesImpuesto [2]
+	alert((impu) + opimpu);
 
-	impuesto = cantidad1 * cantidad;
-	alert("su impuesto es " + impuesto);
 } else {
-let cantidad21 = parseInt(prompt("ingrese su cantidad 1"));
-let cantidad22= parseInt(prompt("ingrese su cantidad 2"));
-
-descuento = cantidad21 / cantidad22;
-alert("su descuento es " + descuento);
+let descuento1 = parseInt(prompt("ingrese el descuento a recibir"));
+let descuento2= parseInt(prompt("ingrese el precio de su objeto"));
+let: operacionesDescuento = [
+descuentoR1 = descuento1 / 100,
+descuentoR2 = descuento2 * descuentoR1,
+descuentoR3 = descuento2 - descuentoR2,
+]
+let desc = calculos [0]
+opdescu = operacionesDescuento [2]
+alert((desc) + opdescu);
 }}}}else{
     alert("gracias!")
+	
 }
 }
-
-comprarproducto();
+calculos();
