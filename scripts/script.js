ea60b33d7c18bf0c52d71abbff45361f34ee0102
Angel Roberto <gelosgr@gmail.com>
2023-02-01 16:55:04 -0600

    añadi modals y local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,36 @@
 let impuesto_Btn = document.getElementById(`impuesto`)
-impuesto_Btn.addEventListener(`click`, bienvenida)
-let descuento_Btn =document.getElementById("impuesto")
-descuento_Btn.addEventListener("click", descuento_neto)
+impuesto_Btn.addEventListener(`click`, ()=>{
+	modal.showModal();
+})
+let descuento_Btn = document.getElementById(`descuento`)
+descuento_Btn.addEventListener(`click`, ()=>{
+	modal1.showModal();
+})
+let cerrar_Modal = document.getElementById("cerrarmodal")
+cerrar_Modal.addEventListener("click",() =>{
+modal.close();
+})
+let cerrar_Modal1 = document.getElementById("cerrarmodal1")
+cerrar_Modal1.addEventListener("click",() =>{
+modal1.close();
+})
+let calculoImpuesto_Btn = document.getElementById("calculoimpuesto")
+calculoImpuesto_Btn.addEventListener("click",() =>{
+impuesto_neto();
+})
+let calculoDescuento_Btn = document.getElementById("calculodescuento")
+calculoDescuento_Btn.addEventListener("click",() =>{
+descuento_neto();
+})
+bienvenida();
 function bienvenida(){
+
+	let seleccion = alert("bienvenido a calculadora de impuestos y descuentos!")
+}
+function impuesto_neto (){
 	let calculos = [
 		" una vez ocurrido el descuento su objeto cuesta ", "una vez añadido el impuesto su objeto cuesta ", "el impuesto neto a su objeto es " , "el descuento neto a su objeto es "
 	];
-
-	let seleccion = prompt("bienvenido a calculadora de impuestos y descuentos! ingrese si o no").toUpperCase();
-	while (seleccion != "SI" && seleccion != "NO") {
-	alert("Dato incorrecto, por favor ingrese si o no").toUpperCase();
-
-}if (seleccion === "SI") {
-	alert("bienvenido!");
-	let impuestoDescuento = true;
-	while (impuestoDescuento) {
-		do {
-		calculo = prompt("¿necesitas calcular un descuento un impuesto?");
-	
-		switch (calculo) {
-		case "descuento" || "DESCUENTO":
-		break;
-		case "impuesto" || "IMPUESTO":
-		break;
-		default:
-		alert("no es una opcion elegible");
-		if (alert= true ){
-			stop
-		}
-		}
-		} while (calculo != "descuento" && calculo != "DESCUENTO" && calculo != "impuesto" && calculo != "IMPUESTO");{
-		if (calculo === "impuesto") {
-			impuesto_neto();
-		}else{
-			descuento_neto()
-		};
-function impuesto_neto (){
   let datosImpuesto = {
     tipodeoperacionimpu1:"division",
     tipodeoperacionimpu2:"multiplicacion",
@@ -47,16 +42,17 @@ function impuesto_neto (){
     impuestoR2 = datosImpuesto.impuesto2 * impuestoR1,
     impuestoR3 = datosImpuesto.impuesto2 + impuestoR2,
     ];
-    
-		let impu1= calculos [2]
-		let impu2= operacionesimpuesto [1]
+	let impu1= calculos [2]
+	let impu2= operacionesimpuesto [1]
     let impuextra1=calculos[1]
     let impuextra2=operacionesimpuesto[2] 
-		alert(impu1+impu2)
+	alert(impu1+impu2)
+	localStorage.setItem("resultado", impuextra2);
+	let prueba = localStorage.getItem("resultado")
+	console.log(prueba)
     let seleccion = prompt("¿desea calculcar el impuesto añadido a su objeto?").toUpperCase();
 	while (seleccion != "SI" && seleccion != "NO") {
 	alert("Dato incorrecto, por favor ingrese si o no").toUpperCase();
-
 }if (seleccion === "SI"){
   alert(impuextra1+impuextra2);
 }else{
@@ -64,6 +60,9 @@ function impuesto_neto (){
 }
 }
 function descuento_neto (){
+	let calculos = [
+		" una vez ocurrido el descuento su objeto cuesta ", "una vez añadido el impuesto su objeto cuesta ", "el impuesto neto a su objeto es " , "el descuento neto a su objeto es "
+	];
 	let datosdescuento = {
 		tipodeoperaciondescu1:"division",
 		tipodeoperaciondescu2:"multiplicacion",
@@ -80,6 +79,9 @@ function descuento_neto (){
   let descuextra1= calculos[0]
   let descuextra2=operacionesdescuento[2]
 	alert(descu1+descu2)
+	localStorage.setItem("resultado", descuextra2);
+	let prueba = localStorage.getItem("resultado")
+	console.log(prueba)
   let seleccion = prompt("¿desea calculcar el impuesto añadido a su objeto?").toUpperCase();
 	while (seleccion != "SI" && seleccion != "NO") {
 	alert("Dato incorrecto, por favor ingrese si o no").toUpperCase();
@@ -90,5 +92,3 @@ function descuento_neto (){
   stop
 }
 }
-}}}}
-bienvenida()
